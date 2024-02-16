1abbc11ec0f189d5c96e86017a23f3a93cab476a
Pristine <prisvsyo@gmail.com>
2024-02-15 17:54:55 -0800

    struggled 12 volume

--- problems/12-volume.js
+++ problems/12-volume.js
@@ -13,8 +13,34 @@ function returned by recVolume should continue to return the original volume.
 
 ***********************************************************************/
 
-// Your code here 
+// Your code here
+function recVolume(height) {
+  return function(length) {
+    return function(width) {
+      volume = height * length * width;
+      return volume;
+    }
+  }
+}
 
+/*
+function recVolume(height) {
+  let volume = [height];
+
+  return function (measurement) {
+    volume.push(measurement)
+
+    if (volume.length < 3) {
+      return recVolume;
+    } else {
+      const [height, width, length] = volume;
+      return height * width * length;
+    }
+
+  }
+
+}
+*/
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
